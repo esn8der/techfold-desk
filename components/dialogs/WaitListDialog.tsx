efbf42a40d1da9ebed98311c9ab799845a8dec76
esn8der <ballesta.esneider@gmail.com>
2023-11-08 21:10:34 -0500

    Add MUI, Emotion, and Notistack dependencies and implement a WaitListDialog component

---
+++ components/dialogs/WaitListDialog.tsx
@@ -0,0 +1,114 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Slide,
+  TextField,
+} from "@mui/material";
+import { TransitionProps } from "@mui/material/transitions";
+import { enqueueSnackbar } from "notistack";
+import React, { useState } from "react";
+import { BiRightArrowAlt } from "react-icons/bi";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+const WaitListDialog = () => {
+  const [isDialogOpen, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleSubmit = () => {
+    handleClose();
+    enqueueSnackbar("Mensaje Enviado!", { variant: "success" });
+  };
+
+  return (
+    <div>
+      <Button
+        sx={{
+          mt: "2.5rem",
+        }}
+        className="group relative overflow-clip inline-flex w-fit mt-10 items-center justify-center h-10 px-7 text-sm font-medium tracking-wide text-white transition duration-300 rounded whitespace-nowrap bg-teal-500 hover:bg-teal-600 focus:bg-teal-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-teal-300 disabled:bg-teal-300 disabled:shadow-none"
+        onClick={handleOpen}
+      >
+        <BiRightArrowAlt className="absolute mr-2 text-xl -translate-x-24 group-hover:-translate-x-14 transition-all duration-300 ease-in scale-50 group-hover:scale-100" />
+        <span className="-translate-x-3 group-hover:translate-x-4 transition-transform duration-300 ease-in">
+          Lista de espera
+        </span>
+        <BiRightArrowAlt className="absolute ml-2 text-xl translate-x-14 group-hover:translate-x-24 group-hover:scale-50 transition-all duration-300 ease-in" />
+      </Button>
+      <Dialog
+        open={isDialogOpen}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        maxWidth={"xs"}
+      >
+        <DialogTitle>Unirse a la lista de espera</DialogTitle>
+        <DialogContent className="flex flex-col gap-4">
+          <DialogContentText>
+            Por favor, completa el formulario.
+          </DialogContentText>
+          <form>
+            {/* <TextField
+                fullWidth
+                label="Nombre"
+                name="name"
+                autoComplete="name"
+                variant="filled"
+                margin="normal"
+                required
+              /> */}
+            <TextField
+              fullWidth
+              label="Email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              variant="filled"
+              margin="normal"
+              required
+            />
+            <TextField
+              fullWidth
+              label="¿Por qué quieres un TechFold Desk?"
+              name="message"
+              multiline
+              rows={4}
+              variant="filled"
+              margin="normal"
+              required
+            />
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cerrar</Button>
+          <Button
+            onClick={handleSubmit}
+            className="bg-gradient-to-l from-gradient-primary to-[#00d68f]"
+          >
+            Enviar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+export { WaitListDialog };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,112 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "@/components/layout";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Inter } from "next/font/google";
+import { SnackbarProvider } from "notistack";
+
+const inter = Inter({ subsets: ["latin"] });
+
+const theme = createTheme({
+  typography: {
+    fontFamily: inter.style.fontFamily,
+    fontWeightBold: 700,
+  },
+  palette: {
+    primary: {
+      main: "#00d68f",
+    },
+    secondary: {
+      main: "#005639",
+    },
+    background: {
+      paper: "#1f2933",
+    },
+    text: {
+      primary: "#fff",
+      secondary: "#C4C4FF",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          color: "#fff",
+          borderRadius: "8px",
+          fontWeight: "bold",
+          transition: "all 200ms ease-in",
+          padding: "0.75rem 1rem",
+          "&:hover": {
+            transform: "scale(1.05)",
+          },
+        },
+      },
+    },
+    MuiDialogTitle: {
+      styleOverrides: {
+        root: {
+          fontSize: "1.875rem",
+          lineHeight: "2.25rem",
+          fontWeight: "bold",
+          borderWidth: "2px",
+          borderColor: "#374151",
+          borderBottomWidth: "0px",
+          textAlign: "center",
+          color: "#fff",
+        },
+      },
+    },
+    MuiDialogContent: {
+      styleOverrides: {
+        root: {
+          borderWidth: "2px",
+          borderColor: "#374151",
+          borderTopWidth: "0px",
+          borderBottomWidth: "0px",
+          backgroundColor: "#1f2933",
+        },
+      },
+    },
+    MuiDialogContentText: {
+      styleOverrides: {
+        root: {
+          textAlign: "center",
+          color: "#C4C4FF",
+        },
+      },
+    },
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#283542",
+          "& .MuiFilledInput-root": {
+            backgroundColor: "#283542",
+          },
+        },
+      },
+    },
+    MuiDialogActions: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#1f2933",
+          gap: "0.5rem",
+          borderWidth: "2px",
+          borderColor: "#374151",
+          borderTopWidth: "0px",
+        },
+      },
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
+      <Layout>
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Layout>
+    </SnackbarProvider>
   );
 }
